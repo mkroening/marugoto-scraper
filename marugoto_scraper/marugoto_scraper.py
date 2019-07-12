4144c09d01e98028ee04d2927be8478ab67cae02
Martin Kröning <m.kroening@hotmail.de>
2019-07-12 16:39:34 +0200

    Use underscores for variables

--- marugoto_scraper/marugoto_scraper.py
+++ marugoto_scraper/marugoto_scraper.py
@@ -14,27 +14,27 @@ logging.basicConfig(level=logging.INFO)
 
 delimiter = '|'
 
-baseURL = 'https://words.marugotoweb.jp/'
-baseName = 'MARUGOTO-NO-KOTOBA'
+base_url = 'https://words.marugotoweb.jp/'
+base_name = 'MARUGOTO-NO-KOTOBA'
 
-levelIDs = [
+level_ids = [
     'A1',
     'A2-1',
     'A2-2'
 ]
 
 
-def levelQuery(levelID: str) -> str:
-    return '&lv=' + levelID
+def level_query(level_id: str) -> str:
+    return '&lv=' + level_id
 
 
-topicQuery = '&tp=1,2,3,4,5,6,7,8,9'
+topic_query = '&tp=1,2,3,4,5,6,7,8,9'
 
-lessonQuery = '&ls=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18'
+lesson_query = '&ls=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18'
 
-textQuery = '&tx=act,comp,vocab'
+text_query = '&tx=act,comp,vocab'
 
-languageIDs = [
+language_ids = [
     'en',
     'es',
     'id',
@@ -45,113 +45,113 @@ languageIDs = [
 ]
 
 
-def languageQuery(languageID: str) -> str:
-    return '&ut=' + languageID
+def language_query(language_id: str) -> str:
+    return '&ut=' + language_id
 
 
-def wordsURL(languageID: str, levelID: str) -> str:
-    return baseURL + 'SearchCategoryAPI?' + levelQuery(levelID) + topicQuery + lessonQuery + textQuery + languageQuery(languageID)
+def words_url(language_id: str, level_id: str) -> str:
+    return base_url + 'SearchCategoryAPI?' + level_query(level_id) + topic_query + lesson_query + text_query + language_query(language_id)
 
 
-audioPathPrefix = '/res/keyword/audio/'
-audioPattern = re.compile('^([^_]+)-([0-9]+)$')
-audioExtension = '.mp3'
+audio_path_prefix = '/res/keyword/audio/'
+audio_pattern = re.compile('^([^_]+)-([0-9]+)$')
+audio_extension = '.mp3'
 
 
-def audioFilename(rawID: str) -> str:
-    return audioPattern.sub(r'\1W_\2', rawID) + audioExtension
+def audio_filename(raw_id: str) -> str:
+    return audio_pattern.sub(r'\1W_\2', raw_id) + audio_extension
 
 
-def getAudioPath(rawID: str) -> str:
-    return audioPathPrefix + audioPattern.sub(r'\1W/\1W_\2', rawID) + audioExtension
+def get_audio_path(raw_id: str) -> str:
+    return audio_path_prefix + audio_pattern.sub(r'\1W/\1W_\2', raw_id) + audio_extension
 
 
-def extractTags(attributes: Dict[str, Dict[str, str]]) -> List[str]:
-    mappedAttributes = [[
+def extract_tags(attributes: Dict[str, Dict[str, str]]) -> List[str]:
+    mapped_attributes = [[
         attribute['level'],
         attribute['utext'],
         'Topic' + attribute['topic'],
         'Lesson' + attribute['lesson']
     ] for attribute in attributes]
-    flatAttributes = [
-        attribute for sublist in mappedAttributes for attribute in sublist]
-    return sorted(list(set(flatAttributes)))
+    flat_attributes = [
+        attribute for sublist in mapped_attributes for attribute in sublist]
+    return sorted(list(set(flat_attributes)))
 
 
-def extractRows(jsonRep: dict) -> List[List[str]]:
-    for word in jsonRep['DATA']:
+def extract_rows(json_rep: dict) -> List[List[str]]:
+    for word in json_rep['DATA']:
         yield [
             word['RAWID'],
             word['KANA'],
             word['KANJI'],
             word['ROMAJI'],
             word['UWRD'],
-            '[sound:' + audioFilename(word['RAWID']) + ']',
-            ' '.join(extractTags(word['ATTR']))
+            '[sound:' + audio_filename(word['RAWID']) + ']',
+            ' '.join(extract_tags(word['ATTR']))
         ]
 
 
-def isDownloaded(onlineFileInfo, path: str) -> bool:
+def is_downloaded(online_file_info, path: str) -> bool:
     if not os.path.isfile(path):
         return False
-    localFileSize = os.path.getsize(path)
-    onlineFileSize = int(onlineFileInfo['Content-Length'])
-    if localFileSize != onlineFileSize:
+    local_file_size = os.path.getsize(path)
+    online_file_size = int(online_file_info['Content-Length'])
+    if local_file_size != online_file_size:
         return False
-    localModifiedTime = os.path.getmtime(path)
-    onlineModifiedTime = time.mktime(
-        email.utils.parsedate(onlineFileInfo['Last-Modified']))
-    if localModifiedTime != onlineModifiedTime:
+    local_modified_time = os.path.getmtime(path)
+    online_modified_time = time.mktime(
+        email.utils.parsedate(online_file_info['Last-Modified']))
+    if local_modified_time != online_modified_time:
         return False
     return True
 
 
-def downloadAudio(rawID: str, basePath: str) -> None:
-    onlineURL = baseURL + getAudioPath(rawID)
-    if not os.path.isdir(basePath):
-        os.makedirs(basePath)
-    localPath = os.path.join(basePath, audioFilename(rawID))
-    with urllib.request.urlopen(onlineURL) as onlineAudioFile:
-        onlineFileInfo = onlineAudioFile.info()
-        if not isDownloaded(onlineFileInfo, localPath):
-            logging.info('Downloading ' + localPath)
-            onlineModifiedTime = time.mktime(
-                email.utils.parsedate(onlineFileInfo['Last-Modified']))
-            with open(localPath, 'wb') as localAudioFile:
-                shutil.copyfileobj(onlineAudioFile, localAudioFile)
-            os.utime(localPath, (onlineModifiedTime, onlineModifiedTime))
+def download_audio(raw_id: str, base_path: str) -> None:
+    online_url = base_url + get_audio_path(raw_id)
+    if not os.path.isdir(base_path):
+        os.makedirs(base_path)
+    local_path = os.path.join(base_path, audio_filename(raw_id))
+    with urllib.request.urlopen(online_url) as online_audio_file:
+        online_file_info = online_audio_file.info()
+        if not is_downloaded(online_file_info, local_path):
+            logging.info('Downloading ' + local_path)
+            online_modified_time = time.mktime(
+                email.utils.parsedate(online_file_info['Last-Modified']))
+            with open(local_path, 'wb') as local_audio_file:
+                shutil.copyfileobj(online_audio_file, local_audio_file)
+            os.utime(local_path, (online_modified_time, online_modified_time))
         else:
-            logging.debug('Already downloaded ' + localPath)
+            logging.debug('Already downloaded ' + local_path)
 
 
-def downloadAllAudio(jsonRep, basePath: str) -> None:
-    logging.info('Starting audio downloads for level ' + jsonRep['LV'])
-    for word in jsonRep['DATA']:
+def download_all_audio(json_rep, base_path: str) -> None:
+    logging.info('Starting audio downloads for level ' + json_rep['LV'])
+    for word in json_rep['DATA']:
         try:
-            downloadAudio(word['RAWID'], basePath)
+            download_audio(word['RAWID'], base_path)
         except urllib.error.HTTPError:
             logging.warning('Could not download ' +
-                            audioFilename(word['RAWID']))
-    logging.info('Audio downloads completed for level ' + jsonRep['LV'])
+                            audio_filename(word['RAWID']))
+    logging.info('_audio downloads completed for level ' + json_rep['LV'])
 
 
-basePath = 'words'
-for languageID in languageIDs:
-    for levelID in levelIDs:
-        if not os.path.isdir(basePath):
-            os.makedirs(basePath)
-        localPath = os.path.join(
-            basePath, baseName + '-' + languageID + '-' + levelID + '.csv')
-        logging.info('Exporting ' + languageID + '-' + levelID)
-        with urllib.request.urlopen(wordsURL(languageID, levelID)) as inputJSONFile:
-            jsonRep = json.loads(inputJSONFile.read().decode('utf-8'))
-        rows = extractRows(jsonRep)
-        with open(localPath, 'w') as outputCSVFile:
-            writer = csv.writer(outputCSVFile, delimiter=delimiter)
+base_path = 'words'
+for language_id in language_ids:
+    for level_id in level_ids:
+        if not os.path.isdir(base_path):
+            os.makedirs(base_path)
+        local_path = os.path.join(
+            base_path, base_name + '-' + language_id + '-' + level_id + '.csv')
+        logging.info('Exporting ' + language_id + '-' + level_id)
+        with urllib.request.urlopen(words_url(language_id, level_id)) as input_json_file:
+            json_rep = json.loads(input_json_file.read().decode('utf-8'))
+        rows = extract_rows(json_rep)
+        with open(local_path, 'w') as output_csv_file:
+            writer = csv.writer(output_csv_file, delimiter=delimiter)
             writer.writerows(rows)
-        logging.info('Exported to ' + localPath)
+        logging.info('Exported to ' + local_path)
 
-for levelID in levelIDs:
-    with urllib.request.urlopen(wordsURL('en', levelID)) as inputJSONFile:
-        jsonRep = json.loads(inputJSONFile.read().decode('utf-8'))
-    downloadAllAudio(jsonRep, os.path.join('media', baseName + '-' + levelID))
+for level_id in level_ids:
+    with urllib.request.urlopen(words_url('en', level_id)) as input_json_file:
+        json_rep = json.loads(input_json_file.read().decode('utf-8'))
+    download_all_audio(json_rep, os.path.join('media', base_name + '-' + level_id))
