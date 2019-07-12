bdd95801d6b171466ab171057872644c9e60213e
Martin Kröning <m.kroening@hotmail.de>
2019-07-12 21:38:31 +0200

    Merge multiple levels

--- marugoto_scraper/marugoto_scraper.py
+++ marugoto_scraper/marugoto_scraper.py
@@ -9,7 +9,7 @@ import shutil
 import time
 from typing import Dict, Iterable, Iterator, List, Sequence
 
-from requests import Session
+import requests
 from requests.exceptions import HTTPError
 from requests_futures.sessions import FuturesSession
 
@@ -23,11 +23,13 @@ base_name = 'MARUGOTO-NO-KOTOBA'
 words_api_url = base_url + '/SearchCategoryAPI'
 
 
-def words_api_params(language_id: str,
-                     level_id: str,
+def words_api_params(language_id: str = 'en',
+                     level_ids: Sequence[str] = None,
                      topics: Sequence[int] = None,
                      lessons: Sequence[int] = None,
                      texts: Sequence[str] = None) -> Dict[str, str]:
+    if level_ids is None:
+        level_ids = ['A1', 'A2-1', 'A2-2']
     if topics is None:
         topics = range(1, 10)
     if lessons is None:
@@ -35,7 +37,7 @@ def words_api_params(language_id: str,
     if texts is None:
         texts = ['act', 'comp', 'vocab']
     return {
-        'lv': level_id,
+        'lv': ','.join(level_ids),
         'tp': ','.join(str(topic) for topic in topics),
         'ls': ','.join(str(lesson) for lesson in lessons),
         'tx': ','.join(texts),
@@ -122,44 +124,28 @@ def download_audio(raw_ids: Iterable[str], prefix: str) -> None:
             logging.warning('Could not download ' + audio_filename(raw_id))
 
 
-available_level_ids = ['A1', 'A2-1', 'A2-2']
-
 available_language_ids = ['en', 'es', 'id', 'th', 'zh', 'vi', 'fr']
 
 
-def download_words(language_id: str,
-                   level_ids: Sequence[str] = available_level_ids) -> None:
+def download_words(language_id: str) -> None:
     base_path = 'words'
-    session = FuturesSession()
-    futures = {
-        level_id: session.get(words_api_url,
-                              params=words_api_params(language_id, level_id))
-        for level_id in level_ids
-    }
-    for level_id, future in futures.items():
-        if not os.path.isdir(base_path):
-            os.makedirs(base_path)
-        local_path = os.path.join(
-            base_path, base_name + '-' + language_id + '-' + level_id + '.csv')
-        logging.info('Exporting ' + language_id + '-' + level_id)
-        json_rep = future.result().json()
-        rows = extract_rows(json_rep)
-        with open(local_path, 'w') as output_csv_file:
-            writer = csv.writer(output_csv_file, delimiter=delimiter)
-            writer.writerows(rows)
-        logging.info('Exported to ' + local_path)
+    if not os.path.isdir(base_path):
+        os.makedirs(base_path)
+    local_path = os.path.join(base_path,
+                              base_name + '-' + language_id + '.csv')
+    logging.info('Exporting ' + language_id)
+    r = requests.get(words_api_url, params=words_api_params())
+    rows = extract_rows(r.json())
+    with open(local_path, 'w') as output_csv_file:
+        writer = csv.writer(output_csv_file, delimiter=delimiter)
+        writer.writerows(rows)
+    logging.info('Exported to ' + local_path)
 
 
-def download_audios(level_ids: Sequence[str] = available_level_ids) -> None:
-    session = Session()
-    for level_id in level_ids:
-        json_rep = session.get(words_api_url,
-                               params=words_api_params('en', level_id)).json()
-        prefix = os.path.join('media', base_name + '-' + level_id)
-        raw_ids = [word['RAWID'] for word in json_rep['DATA']]
-        logging.info('Starting audio downloads for level ' + level_id)
-        download_audio(raw_ids, prefix)
-        logging.info('_audio downloads completed for level ' + level_id)
+def download_audios() -> None:
+    r = requests.get(words_api_url, params=words_api_params())
+    raw_ids = [word['RAWID'] for word in r.json()['DATA']]
+    download_audio(raw_ids, 'media')
 
 
 if __name__ == '__main__':
